f6eaf5fb75dc744259b53e0f5fa4ac0931bf77a2
kwiniarski97 <kwiniarski97@gmail.com>
2018-05-01 17:39:44 +0200

    add post form

--- Knowledge.WebSite/src/app/app.module.ts
+++ Knowledge.WebSite/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { SearchboxComponent } from './components/searchbox/searchbox.component';
 import { AddComponent } from './components/add/add.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatInputModule} from '@angular/material';
+import {MatButtonModule, MatIconModule, MatInputModule} from '@angular/material';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
+import {CovalentFileModule, CovalentLayoutModule, CovalentStepsModule} from '@covalent/core';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -18,14 +21,20 @@ import {MatButtonModule, MatInputModule} from '@angular/material';
     HomeComponent,
     SearchboxComponent,
     AddComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    SearchResultsComponent
 
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatButtonModule,
+    CovalentLayoutModule,
+    CovalentStepsModule,
+    CovalentFileModule,
+    MatIconModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'add', component: AddComponent },
--- Knowledge.WebSite/src/app/components/add/add.component.ts
+++ Knowledge.WebSite/src/app/components/add/add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-add',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddComponent implements OnInit {
 
-  constructor() { }
+  model: any = {};
+  files: any;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
